7a124edb27d422034a58b0e9532deb0c4ddc854a
Oksana Cherviachenko <kvintinovna@gmail.com>
2024-11-29 18:13:40 +0100

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store'; // Додаємо persistor
-import { PersistGate } from 'redux-persist/integration/react'; // Імпортуємо PersistGate
+import { store, persistor } from './redux/store'; 
+import { PersistGate } from 'redux-persist/integration/react'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
